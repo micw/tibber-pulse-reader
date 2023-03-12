5f81a3f5d3247ab4448913455ca7a9bfd1e55cb8
Michael Wyraz <michael@wyraz.de>
2023-03-12 17:36:52 +0100

    Better error reporting

--- src/main/java/de/wyraz/tibberpulse/TibberPulseReader.java
+++ src/main/java/de/wyraz/tibberpulse/TibberPulseReader.java
@@ -1,5 +1,8 @@
 package de.wyraz.tibberpulse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +11,32 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TibberPulseReader {
 
+	protected static final Logger log = LoggerFactory.getLogger(TibberPulseReader.class);
+	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(TibberPulseReader.class, args);
+		try {
+			SpringApplication.run(TibberPulseReader.class, args);
+		} catch (BeanCreationException ex) {
+			Throwable th=ex;
+			while (th.getCause()!=null) {
+				th=th.getCause();
+			}
+			StringBuilder error=new StringBuilder();
+			error.append("\n");
+			error.append("\n");
+			error.append("Application startup failed\n");
+			error.append("===========================\n");
+			error.append("\n");
+			error.append(th.getMessage()).append("\n");
+			error.append("\n");
+					
+			if (log.isErrorEnabled()) {
+				log.error(error.toString());
+			} else {
+				System.err.println(error.toString());
+			}
+		}
+		System.exit(0);
 	}
 
 }
